fb51b572a24073752bff5b49fbcbb24d131209d7
kyle-mccurdy <mccurdy@iastate.edu>
2025-05-06 14:49:43 -0500

    hamburger menu

--- grid.js
+++ grid.js
@@ -1,12 +1,11 @@
-function navToggle() { // creates a new function named navToggle
-  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
-    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
-    x.className += "responsive";   // then add a new class
-  } else {                          // if it doesn't,
-    x.className = "topnav";         // set class to .topnav
-  } //closes if-else
-} //closes function
-
+function navToggle() { 
+  var x = document.getElementById("main-nav"); 
+    if (x.className === "topnav") { 
+    x.className += "responsive";   
+  } else {                          
+    x.className = "topnav";         
+  } 
+} 
 
 
 $(".button").on("click", function() {
